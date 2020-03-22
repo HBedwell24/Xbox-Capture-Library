158cf9b87cae6299efa547a2ecf7bcacc703b046
Hunter Bedwell <bedwellhb@gmail.com>
2020-03-22 12:52:33 -0400

    Altered the application to adhere to the internal style specified within the Resources folder.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,25 @@
-﻿using System.Windows;
+﻿using MahApps.Metro;
+using System;
+using System.Windows;
 
 namespace XboxGameClipLibrary
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // add custom accent and theme resource dictionaries to the ThemeManager
+            // you should replace MahAppsMetroThemesSample with your application name
+            // and correct place where your custom accent lives
+            ThemeManager.AddAccent("Accent", new Uri("pack://application:,,,/XboxGameClipLibrary;component/Resources/Themes/Accent.xaml"));
+
+            // get the current app style (theme and accent) from the application
+            // Tuple<AppTheme, Accent> theme = ThemeManager.DetectAppStyle(Current);
+
+            // now change app style to the custom accent and current theme
+            ThemeManager.ChangeAppStyle(Current, ThemeManager.GetAccent("Accent"), ThemeManager.GetAppTheme("BaseDark"));
+
+            base.OnStartup(e);
+        }
     }
 }
