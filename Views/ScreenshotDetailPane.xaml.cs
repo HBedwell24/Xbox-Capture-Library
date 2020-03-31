9f0653884cd66809c589609df39b2aed04f2d44a
Hunter Bedwell <bedwellhb@gmail.com>
2020-03-30 23:29:54 -0400

    Simplified XAML for screenshot list items.

--- Views/ScreenshotDetailPane.xaml.cs
+++ Views/ScreenshotDetailPane.xaml.cs
@@ -5,9 +5,9 @@ namespace XboxGameClipLibrary.Views
     /// <summary>
     /// Interaction logic for DetailPane.xaml
     /// </summary>
-    public partial class DetailPane : UserControl
+    public partial class ScreenshotDetailPane : UserControl
     {
-        public DetailPane()
+        public ScreenshotDetailPane()
         {
             InitializeComponent();
         }
