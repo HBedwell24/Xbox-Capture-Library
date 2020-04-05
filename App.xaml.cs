de2881bd04373751ee5c264b9447103c4dd22bc8
Hunter Bedwell <bedwellhb@gmail.com>
2020-04-04 23:24:28 -0400

    Added a splash screen to the application hierarchy.

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,23 +6,10 @@ namespace XboxGameClipLibrary
 {
     public partial class App : Application
     {
-        readonly string credential = XboxGameClipLibrary.Properties.Settings.Default.xboxApiKey;
-
         protected override void OnStartup(StartupEventArgs e)
         {
             ThemeManager.AddAccent("Accent", new Uri("pack://application:,,,/XboxGameClipLibrary;component/Resources/Themes/Accent.xaml"));
             ThemeManager.ChangeAppStyle(Current, ThemeManager.GetAccent("Accent"), ThemeManager.GetAppTheme("BaseDark"));
-
-            if (string.IsNullOrEmpty(credential))
-            {
-                EnterUserDetails enterUserDetails = new EnterUserDetails();
-                enterUserDetails.Show();
-            }
-            else
-            {
-                MainWindow mainWindow = new MainWindow();
-                mainWindow.Show();
-            }
             base.OnStartup(e);
         }
     }
---
+++ SplashScreen.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace XboxGameClipLibrary
+{
+    public partial class SplashScreen : Window
+    {
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        private readonly string credential = Properties.Settings.Default.xboxApiKey;
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+
+            dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
+            dispatcherTimer.Start();
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(credential))
+            {
+                EnterUserDetails enterUserDetails = new EnterUserDetails();
+                enterUserDetails.Show();
+                dispatcherTimer.Stop();
+                Close();
+            }
+            else
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                dispatcherTimer.Stop();
+                Close();
+            }
+        }
+    }
+}
