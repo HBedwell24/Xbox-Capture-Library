0054146ea7c09f02f6f2abdaa028e364e8f16a9e
Christian Bedwell <Christian.8edwell@gmail.com>
2020-04-10 20:32:32 -0400

    Renamed ScreenshotListItem to CaptureListItem and reformatted thumbnail shape to rectangle instead of circle.

---
+++ Views/User Controls/CaptureListItem.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace XboxGameClipLibrary.Views
+{
+    /// <summary>
+    /// Interaction logic for ScreenshotListItem.xaml
+    /// </summary>
+    public partial class CaptureListItem : UserControl
+    {
+        public CaptureListItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
