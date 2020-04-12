b84c3522f378320751e8085ecedb2f3ad4d0c1b5
Hunter Bedwell <bedwellhb@gmail.com>
2020-04-12 13:06:59 -0400

    Began implementation of notifications for informing the user of download progress.

---
+++ View Models/CustomNotificationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+
+namespace XboxGameClipLibrary.View_Models
+{
+    [POCOViewModel]
+    public class CustomNotificationViewModel
+    {
+        public virtual string Caption { get; set; }
+        public virtual string Content { get; set; }
+    }
+}
